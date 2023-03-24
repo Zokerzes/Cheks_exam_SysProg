ce147c9fa30673d95f2456e54d573e33616f2d08
Zokerzes <Bliz_mw86@itstepru.onmicrosoft.com>
2023-03-24 20:50:39 +0300

    ++

--- Cheks/MainModel.cs
+++ Cheks/MainModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Cheks
@@ -11,6 +12,8 @@ namespace Cheks
     internal class MainModel : INPC
     {
         const string FORB_FILE = "BlokList.txt";
+        const string COPY_DIRECT_NAME = "Forbfidden Files";
+
         private string _selectedDirectory;
         public string SelectedDirectory
         {
@@ -65,8 +68,61 @@ namespace Cheks
             foreach (var file in files)
             {
 
+                using (StreamReader stream = new StreamReader(file))
+                {
+                    string line;
+                    bool find = false;
+                    while ((line = await stream.ReadLineAsync()) != null && !find)
+                    {
+                        foreach (var word in ForBiddenWords)
+                        {
+                            if (line
+                                .ToLower()
+                                .Contains(word.ToLower()))
+                            {
+                                find = true;
+                                var fi = new FileInfo(file);
+                                File.Copy(file, Environment.GetFolderPath(Environment
+                                    .SpecialFolder.MyDocuments) + $"\\{COPY_DIRECT_NAME}\\{fi.Name}_forbidden");
+                                break;
+                            }
+                        }
+
+
+                    }
+                }
+
             }
         }
+        //Заменить все слова на звездочки
+        public async Task ReplaceTextFiles()
+        {
+            List<string> files = Directory.GetFiles(Environment.GetFolderPath(Environment
+                                    .SpecialFolder.MyDocuments) + $"\\{COPY_DIRECT_NAME}").ToList();
+            foreach (var file in files)
+            {
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    string line;
+                    while ((line = await sr.ReadLineAsync()) != null)
+                    {
+                        foreach (var word in ForBiddenWords)
+                        {
+                            if (line.ToLower()
+                               .Contains(word.ToLower()))
+                            {
+                                var replasedLine = Regex.Replace(line, word, new string('*', word.Length), RegexOptions.IgnoreCase);
 
+                                using (StreamWriter sw = new StreamWriter(sr.BaseStream))
+                                {
+                                    sw.WriteLine(replasedLine);
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
     }
-}
